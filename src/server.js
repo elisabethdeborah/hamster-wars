55eedd58e75e16cc432f55f7a4d76313fc525ae7
Elisabeth Rumar <elisabeth_deborah@hotmail.com>
2021-09-15 11:28:30 +0200

    server setup

--- src/server.js
+++ src/server.js
@@ -1,27 +1,14 @@
 
 const express = require('express')
 const app = express()
-const hamstersRouter = require('./routes/hamsters.js')
 
 
 //konfigurera
 const PORT = process.env.PORT || 1337
 
-//middleware. //middleware ska alltid ha tre parametrar
-//logger, bra att se alla requests
-app.use( express.urlencoded( {extended: true}) ) //används vid autentisering/vill skicka formulär 
+//middleware
+app.use( express.urlencoded( {extended: true}) ) 
 app.use ( express.json() )
-/* app.use((req, res, next) => {
-    console.log(`Method: ${req.method} Url: ${req.url}, Body: ${req.body}`);
-    next()
-}) */
-
-
-
-
-//routes / endpoints
-//hamstersrouter-middleware ska användas av alla routes som börjar med hamsters.
-//app.use('/hamsters', hamstersRouter) 
 
 
 
